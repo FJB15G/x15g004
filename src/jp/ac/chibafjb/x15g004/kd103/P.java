8a1c3bdb4431b5544e9d2db92463424ce9455124
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-02 14:46:11 +0900

    課題３

---
+++ src/jp/ac/chibafjb/x15g004/kd103/P.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g004.kd103;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.print(" 1 石原　康平　");
+System.out.print(" 2 石原　康平　");
+System.out.print(" 3 石原　康平　");
+System.out.print(" 4 石原　康平　");
+System.out.print(" 5 石原　康平　");
+System.out.print(" 6 石原　康平　");
+System.out.print(" 7 石原　康平　");
+System.out.print(" 8 石原　康平　");
+System.out.print(" 9 石原　康平　");
+System.out.println("10 石原　康平　");
+	}
+
+}
