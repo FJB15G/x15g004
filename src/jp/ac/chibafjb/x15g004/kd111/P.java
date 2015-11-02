e9f10a93bdf344d0248cfb00c58ee429ca518955
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-02 15:02:53 +0900

    課題７

---
+++ src/jp/ac/chibafjb/x15g004/kd111/P.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g004.kd111;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<10;i=i+1)
+			System.out.println("石原　康平");
+	}
+
+}
