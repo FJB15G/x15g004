cf883e0e6e9832fe07effd38b7f15fa9447cc5ae
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-02 14:52:59 +0900

    課題５

---
+++ src/jp/ac/chibafjb/x15g004/kd105/P.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g004.kd105;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("氏名：田中　太郎（たなか　たろう）\n趣味：読書\n特技：テニス");
+	}
+
+}
