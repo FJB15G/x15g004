58357723803af7b64640e20ee44fb2df9f6192f5
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-02 14:59:36 +0900

    課題６

---
+++ src/jp/ac/chibafjb/x15g004/kd106/P.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g004.kd106;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("   a@@@a a@@@a");
+System.out.println("  @@@@@@e@@@@@@");
+System.out.println(" (@@@@@@@@@@@@@)");
+System.out.println("  @@@@@@@@@@@@@");
+System.out.println("   @@@@@@@@@@@");
+System.out.println("    ~@@@@@@@~");
+System.out.println("      ~@@@~");
+System.out.println("       ~@~");
+System.out.println("                ハート");
+	}
+
+}
