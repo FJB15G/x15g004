82aac0ef46684b1e3952de917de2a85c37ec69b0
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-02 14:40:28 +0900

    課題２

---
+++ src/jp/ac/chibafjb/x15g004/kd102/P.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g004.kd102;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("1  石原　康平");
+System.out.println("2  石原　康平");
+System.out.println("3  石原　康平");
+System.out.println("4  石原　康平");
+System.out.println("5  石原　康平");
+System.out.println("6  石原　康平");
+System.out.println("7  石原　康平");
+System.out.println("8  石原　康平");
+System.out.println("9  石原　康平");
+System.out.println("10 石原　康平");
+
+	}
+
+}
