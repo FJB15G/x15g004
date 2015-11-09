00bf675f69b1f8c7e8012a22455b1cc5fe826f03
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-09 14:35:30 +0900

    課題２２

---
+++ src/jp/ac/chibafjb/x15g004/kdT03/P.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g004.kdT03;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=55;i<71;i=i+5)
+			System.out.println(i);
+	}
+
+}
