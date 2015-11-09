018a4ea8cd31f0526263f32712b5150cd2b10257
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-09 14:29:35 +0900

    課題１９

---
+++ src/jp/ac/chibafjb/x15g004/kd126/P.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g004.kd126;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<3;i++)
+			System.out.println("12345");
+	}
+
+}
