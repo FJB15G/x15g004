c8b0e5b1cd0df949fcca070232388c436eea13b7
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-09 14:53:59 +0900

    課題２８

---
+++ src/jp/ac/chibafjb/x15g004/kd137/P.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g004.kd137;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		int c;
+		a = 100;
+		b = 1;
+		c = b;
+		b = a;
+		a = c;
+		c = b;
+		System.out.println("a ="+a);
+		System.out.println("b ="+b);
+		System.out.println("c ="+c);
+	}
+
+}
