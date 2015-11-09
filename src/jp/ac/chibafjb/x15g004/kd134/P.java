12ce738d113d3cfdcd08c7f5d85169944aaf2dea
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-09 14:46:07 +0900

    課題２６

---
+++ src/jp/ac/chibafjb/x15g004/kd134/P.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g004.kd134;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		int y;
+		x = 10;
+		y = 20;
+		y = x;
+		System.out.print("x = "+x+"\ny = "+y);
+		
+	}
+
+}
