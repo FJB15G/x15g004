a88bd6c4fbcc2c436c383073ab1548a338ce6dd5
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-09 14:37:10 +0900

    課題２３

---
+++ src/jp/ac/chibafjb/x15g004/kd131/P.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g004.kd131;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		i = 10;
+		System.out.print(i);
+	}
+
+}
