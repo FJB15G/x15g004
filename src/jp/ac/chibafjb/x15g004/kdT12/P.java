3b6c9cb4c2aebc7e27367016d2a178c9c9297d36
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2015-11-16 15:09:23 +0900

    課題３１

---
+++ src/jp/ac/chibafjb/x15g004/kdT12/P.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g004.kdT12;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=30;i>19;i=i-5)
+		System.out.print(i+" ");
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g004/kdT21/P.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g004.kdT21;
+
+import java.util.Scanner;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sin = new Scanner(System.in);
+		System.out.print("ハンバーガーの個数 -->");
+		Scanner sin = new Scanner(System.in);
+		System.out.print("フライドポテトの個数-->");
+	}
+
+}
