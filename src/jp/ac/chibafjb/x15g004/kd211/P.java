646ff2a6f6d008c5465738fd8b586e7311261bda
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2016-01-25 15:02:06 +0900

    作業38

--- src/jp/ac/chibafjb/x15g004/kd211/P.java
+++ src/jp/ac/chibafjb/x15g004/kd211/P.java
@@ -5,21 +5,34 @@ public class P {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int [] num = new int[100];
-		
-		
-		System.out.println(m);
-		}
-	}
-class ArrayList{
-	public int maxFind1(){
-		int a = num.length; 
-	int m = 0;
-	for(int i = 0;i<a;i++){
-		if(num[i] > m){
-			m = num[i];
-		}
-		
-	}
-	return m;
+		int a = num.length;
+		ArrayList x = new ArrayList();
+		System.out.print("配列numの最大値は、"+x.maxFind1(num,a)+"です。\n");
+		System.out.print("配列numの最大値は、"+x.maxFind2(num,a)+"です。\n");
 	}
 }
+	class ArrayList{
+		public int maxFind1(int num[],int a){
+			int m = 0;
+			int i = 0;
+			while(i<a){
+				if(num[i]>m){
+					m = num[i];
+				}
+				i++;
+			}
+		return m;
+		}
+		public int maxFind2(int num[],int a){
+			int i = 1;
+			int m =num[0];
+			while(i<a){
+				if(num[i]>m){
+					m = num[i];
+				}
+				i++;
+			}
+			return m;
+		}
+	}
+
---
+++ src/jp/ac/chibafjb/x15g004/kd221/P.java
@@ -0,0 +1,25 @@
+package jp.ac.chibafjb.x15g004.kd221;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num[] = new int[100];
+		int n = num.length;
+		ArrayList x = new ArrayList();
+		num[99] =999; 
+		Scanner sin = new Scanner(System.in);
+		String s = sin.next();
+		
+		class ArrayProc{
+			public int linerSearch1(int num[],int n){
+				return m;
+			}
+		}
+		
+	}
+
+}
