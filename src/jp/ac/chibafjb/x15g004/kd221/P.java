4510cfd581e353d83aa1703b7f70861ee5a9dae6
unknown <x15g004@PC504N030.chiba-fjb.ac.jp>
2016-02-01 15:11:28 +0900

    作業40

--- src/jp/ac/chibafjb/x15g004/kd221/P.java
+++ src/jp/ac/chibafjb/x15g004/kd221/P.java
@@ -8,25 +8,32 @@ public class P {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int score[] = new int[100];
+		score[99] = 999;
 		int a = score.length;
-		ArrayList x = new ArrayList();
-		score[99] =999; 
-		Scanner sin = new Scanner(System.in);
-		String  n = sin.next();
-		System.out.println("探索値"+n+"は配列score")
-		class ArrayProc{
-			public int linerSearch1(int s[],int n){
-				int i = 0;
-				while(i<100 && i <999){
-					i = i + 1;
+			ArrayList x = new ArrayList();
+			score[99] =999; 
+			Scanner sin = new Scanner(System.in);
+			String  n = sin.next();
+			if(n == score[i]){	
+			System.out.println("探索値"+n+"は配列score["+i+"]に存在します。");
+			else{
+				System.out.println("探索値"+n+"は配列score["+i+"]に存在しません")
 				}
-			return i;	
-					
-			}
 			
-		}
-		}
+				class ArrayProc{
+					public int linerSearch1(int s[],int n){
+						int i = 0;
+						while(i<100 && n !=s[i]){
+							i = i + 1;
+						}
+						return i;	
+					}
+				}
+			
+			}
 		
-	}
-
+		}
 }
+
+
+
